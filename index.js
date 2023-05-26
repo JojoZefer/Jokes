67e8f9b33dbc76527f3b7bd4a529fae3bc5b5b71
JojoZefer <108221802+JojoZefer@users.noreply.github.com>
2023-05-26 19:31:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const url = require('url');
 const dataPath = path.join(__dirname, 'data');
 
 const server = http.createServer((req, res)=> {
-	if(req.url == '/jokes' && req.method == 'GET'){
+	if(req.url == '/jokes' && req.method == 'GET') {
 		getAllJokes(req, res);
 	}
 });
@@ -17,7 +17,7 @@ function getAllJokes(req, res){
 	let dir = fs.readdirSync(dataPath);
 	let allJokes = [];
 	for(let i = 0; i < dir.length; i++){
-		let file = fs.redFileSync(path.join(dataPath, i+'.json'));
+		let file = fs.readFileSync(path.join(dataPath, i+'.json'));
 		let jokeJSON = Buffer.from(file).toString();
 		let joke = JSON.parse(jokeJSON);
 		joke.id = i;
