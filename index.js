6c70839ed108d8d8cb0e25b060cfc61063956dbe
JojoZefer <108221802+JojoZefer@users.noreply.github.com>
2023-06-02 18:26:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const server = http.createServer((req, res)=> {
 	if(req.url == '/jokes' && req.method == 'GET') {
 		getAllJokes(req, res);
 	}
+		if(req.url == '/jokes' && req.method == 'POST') {
+         addJoke(req, res);
+		}
 });
 
 server.listen(3000);
@@ -25,4 +28,23 @@ function getAllJokes(req, res){
 		allJokes.push(joke);
 	}
 	res.end(JSON.stringify(allJokes))
+}
+
+function addJoke(req, res) {
+	let data = '';
+	req.on('data', function(chunk) {
+		data += chunk;
+	});
+	req.end('data', function() {
+     let joke = JSON.parse(data);
+     joke.likes = 0;
+     joke.dislikeke = 0;
+
+     let dir = fs.readdirSync(dataPath);
+     let fileName = dir.length+'.json';
+     let filePath = path.join(dataPath, fileName);
+     fs.writeFileSync(filePath, JSON.stringify(joke));
+
+     res.end();
+	});
 }
